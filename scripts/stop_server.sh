81de56758187bbc6b65f4840100b2411f0fef9e6
Enes Malik Ozer <enes@altogic.com>
2024-05-09 04:29:57 +0300

    chore: Update stop_server.sh script to use PM2 to stop the Fastify application

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Stop the running Fastify server
-if pgrep -f "yakit_asistani" > /dev/null
-then
-  echo "Stopping existing Fastify server"
-  pkill -f "yakit_asistani"
-else
-  echo "No Fastify server running"
-fi
-
  
+APP_NAME="yakit_asistani"
+
+# Check if the application is running using PM2, and stop it if so
+if pm2 list | grep -q "$APP_NAME"; then
+    echo "Stopping the Fastify application..."
+    pm2 stop "$APP_NAME" || echo "Failed to stop $APP_NAME with PM2."
+else
+    echo "No running instance of $APP_NAME found to stop."
+fi
