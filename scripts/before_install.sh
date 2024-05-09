b46794a2b17af96e7dc1536310cad1848e155dd9
Enes Malik Ozer <enes@altogic.com>
2024-05-09 03:00:31 +0300

    chore: Update before_install.sh script to use yum for package updates and installation

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-nvm install --lts
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# source ~/.bashrc
+# nvm install --lts
 
-yum update -y
-yum install -y nodejs npm
+sudo yum update -y
+sudo yum install -y nodejs npm
